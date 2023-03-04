a6baf8cbe35b9ed6ab1b92829f225f0c79a2368d
Zaupower <56695737+Zaupower@users.noreply.github.com>
2023-03-04 12:38:08 +0000

    Add project files.

---
+++ SplitExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace Split
+{
+    public static class SplitExtension
+    {
+        public static ICollection<IEnumerable<T>> Split<T>(this IEnumerable<T> sourceList, int listSize)
+        {
+            ICollection<IEnumerable<T>> listOfLists = new List<IEnumerable<T>>();
+            for (int i = 0; i < sourceList.Count(); i += listSize)
+            {
+                listOfLists.Add(sourceList.Skip(i).Take(listSize));
+            }
+            return listOfLists;
+        }
+
+        public static ICollection<IEnumerable<T>> SplitOrDefault<T>(this IEnumerable<T> sourceList, int listSize)
+        {
+            ICollection<IEnumerable<T>> listOfLists = new List<IEnumerable<T>>();
+            if (listSize > sourceList.Count()) { return listOfLists; }
+            for (int i = 0; i < sourceList.Count(); i += listSize)
+            {
+                listOfLists.Add(sourceList.Skip(i).Take(listSize));
+            }
+            return listOfLists;
+        }
+    }
+}
